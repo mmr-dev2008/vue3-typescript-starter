1e6751058bfbd62cc1819ca4234c2cd7f31bfd86
MohamadRobatjazi <mohamadrobatjazi1387@gmail.com>
2026-06-07 20:05:51 +0330

    feat: create Login and Register views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: []
+    routes: [
+        {
+            path: '/login',
+            name: 'Login',
+            component: () => import('@/views/LoginView.vue')
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            component: () => import('@/views/RegisterView.vue')
+        }
+    ]
 });
 
 export default router;
