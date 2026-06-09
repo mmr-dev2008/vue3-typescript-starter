fba9e36eaf3726c42c373c350703fe748ba337e8
MohamadRobatjazi <mohamadrobatjazi1387@gmail.com>
2026-06-09 13:39:23 +0330

    feat: improvement code

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,5 @@
 </template>
 
 <script setup lang="ts">
-    import * as v from 'valibot';
 
-    v.setGlobalConfig({ lang: 'fa' });
 </script>
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,15 @@
+import * as v from 'valibot';
+
+// Repositories
 import ApiRepository from '@/repositories/api.repository';
 
+// Services
 import LanguageService from '@/services/language.service';
 import TokenService from '@/services/token.service';
 
 import AuthenticateUser from '@/repositories/middleware/AuthenticateUser';
 
+// Enums
 import HttpHeader from '@/enums/HttpHeader';
 import MimeType from '@/enums/MimeType';
 
@@ -13,4 +18,10 @@ ApiRepository.addResponseMiddleware(AuthenticateUser);
 
 if (TokenService.isExist()) {
     ApiRepository.setHeader(HttpHeader.AUTHORIZATION, `Bearer ${TokenService.get()}`);
+}
+
+if (LanguageService.isRtl()) {
+    v.setGlobalConfig({ lang: 'fa' })
+} else {
+    v.setGlobalConfig({ lang: 'en' })
 }
